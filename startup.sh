6426f9fde992771786b35f495909eb40eecdf326
Aleksandr Loktionov <loktionovam@gmail.com>
2018-06-26 21:53:09 +0700

    Added startup.sh for automatic application deployment. Added description of execution startup.sh via the gcloud to README.md (HW-06)

---
+++ startup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#Very simple bootstrap script
+GIT_REPO_URL=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/git_repo_url -H "Metadata-Flavor: Google")
+GIT_REPO_BRANCH=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/git_repo_branch -H "Metadata-Flavor: Google")
+SCRIPTS_LIST="install_ruby.sh install_mongodb.sh deploy.sh"
+
+apt-get update
+apt-get install --yes git
+git clone --no-checkout --branch "${GIT_REPO_BRANCH}" --depth 1 "${GIT_REPO_URL}" startup
+
+pushd startup || exit
+git checkout HEAD generic-functions
+for SCRIPT in ${SCRIPTS_LIST}; do
+  git checkout HEAD "${SCRIPT}"
+  ./"${SCRIPT}"
+done
+popd
+rm -rf startup
