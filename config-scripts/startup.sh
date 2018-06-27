b4ffc5bbb8dea5b3bb7e94d979901b2c0199dfb5
Aleksandr Loktionov <loktionovam@gmail.com>
2018-06-27 12:52:00 +0700

    Add SCRIPTS_DIR to startup.sh, fix broken path to generic-functions (HW-07)

--- config-scripts/startup.sh
+++ config-scripts/startup.sh
@@ -3,15 +3,17 @@
 GIT_REPO_URL=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/git_repo_url -H "Metadata-Flavor: Google")
 GIT_REPO_BRANCH=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/git_repo_branch -H "Metadata-Flavor: Google")
 SCRIPTS_LIST="install_ruby.sh install_mongodb.sh deploy.sh"
+SCRIPTS_DIR='config-scripts'
 
 apt-get update
 apt-get install --yes git
 git clone --no-checkout --branch "${GIT_REPO_BRANCH}" --depth 1 "${GIT_REPO_URL}" startup
 
 pushd startup || exit
-git checkout HEAD config-scripts/generic-functions
+git checkout HEAD "${SCRIPTS_DIR}"/*
+pushd "${SCRIPTS_DIR}" || exit
 for SCRIPT in ${SCRIPTS_LIST}; do
-  git checkout HEAD config-scripts/"${SCRIPT}"
-  config-scripts/"${SCRIPT}"
+  ./"${SCRIPT}"
 done
+popd
 popd
