6a90de9ce276bb710355e590d8250f292d0019d7
Aleksandr Loktionov <loktionovam@gmail.com>
2018-07-18 16:31:25 +0700

    Revert "Terraform 2"

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -5,27 +5,5 @@ set -e
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update
-
 apt-get install -y mongodb-org
-cat > /etc/mongod.conf <<EOF
-storage:
-  dbPath: /var/lib/mongodb
-  journal:
-    enabled: true
-#  engine:
-#  mmapv1:
-#  wiredTiger:
-
-# where to write logging data.
-systemLog:
-  destination: file
-  logAppend: true
-  path: /var/log/mongodb/mongod.log
-
-# network interfaces
-net:
-  port: 27017
-  bindIp: 0.0.0.0
-EOF
-systemctl restart mongod
 systemctl enable mongod
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,29 @@ variable region {
   description = "Region"
   default     = "europe-west1"
 }
+
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable private_key_path {
+  description = "Path to the private key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable "app_backend_port" {
+  description = "Reddit application backend port"
+  default     = 9292
+}
+
+variable "count" {
+  default = 1
+}
